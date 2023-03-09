2dc8eadf10863d09d39d4901ce26556cce6fa66f
Nora Mohammed <NoorMohammed3399@gmail.com>
2023-03-09 20:37:32 +0200

    adding backend and frontend folder and initialize project

---
+++ Backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express'); 
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const PORT = process.env.PORT 
+const DB_URL = process.env.DB_URL 
+
+const server = express()
+server.use(express.json())
+
+
+
+
+
+
+
+
+
+mongoose.connect(DB_URL, (err) => {
+    if (!err) {
+        return console.log("db connected")
+    }
+    console.log("not connected")
+})
+
+server.listen(PORT, (err) => {                                    
+    if (!err) {
+        return console.log("server start")
+    }
+}) 
