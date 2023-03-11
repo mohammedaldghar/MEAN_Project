ebdaede9c67fc90467df8ecadbf74499cf91e4ec
Gehad Hashim <gehad7hashim@outlook.com>
2023-03-12 01:55:35 +0200

    fix error

--- Backend/models/author.js
+++ Backend/models/author.js
@@ -1,9 +1,11 @@
 const mongoose=require('mongoose')
 
-
+//var id = new mongoose.Types.ObjectId();     auth_ID:{type:Number,min:1,required:true},
 const authSchema=new mongoose.Schema({
+    auth_ID:{type:Number,min:1,required:true,unique:true},
     firstName :{type:String,minlength:3 ,maxlength:15,required:true},   
     lastName :{type:String,minlength:3 ,maxlength:15,required:true}, 
+    photo:{type:String},
     DateOfBirth :{type:Date}
     })
     
