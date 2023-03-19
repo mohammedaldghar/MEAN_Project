89ab679bf017923f31c58f58999cbe3339b40405
mohammedaldghar <mohammedaldghar5@gmail.com>
2023-03-19 19:15:15 +0200

    Author modified

--- Backend/models/author.js
+++ Backend/models/author.js
@@ -5,8 +5,8 @@ const authSchema=new mongoose.Schema({
     auth_ID:{type:Number,min:1,required:true,unique:true},
     firstName :{type:String,minlength:3 ,maxlength:15,required:true},   
     lastName :{type:String,minlength:3 ,maxlength:15,required:true}, 
-    photo:{type:String},
-    DateOfBirth :{type:Date}
+    photo:{type:String, required:true},
+    DateOfBirth :{type:Date ,required:true},
     })
     
     const authModel=mongoose.model('author',authSchema)
