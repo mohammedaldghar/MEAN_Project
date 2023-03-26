99db4f7c72b3f8abdbddfd2d4ab13a79d061671b
mohammedaldghar <mohammedaldghar5@gmail.com>
2023-03-26 13:53:27 +0200

    Add user's details to res of login

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -13,30 +13,40 @@ router.post("/", async (req, res) => {
   // }
   // const decoded = jwt.verify(token, TOKEN_KEY);
 
-  const logedUser = await user.findOne({ email: req.body.email });
+  const logedUser = await user
+    .findOne({ email: req.body.email })
+    .populate("readingBooks")
+    .populate("wantToReadBooks")
+    .populate("readBooks");
   if (!logedUser) {
     return res.status(500).send("This Email is not exist");
   }
 
   if (await bcrypt.compare(req.body.password, logedUser.password)) {
     //const allBooks = [...logedUser.readingBooks,...logedUser.wantToReadBooks,...logedUser.readBooks];
-    return res.status(200).send({ message: "Welcome", token: logedUser.token });
+    return res
+      .status(200)
+      .send({ message: "Welcome", token: logedUser.token, user: logedUser });
   } else {
     return res.status(500).send("Incorrect password");
   }
-
 });
 
 router.post("/admin", async (req, res) => {
-  const {email} =req.body;
-  const logedAdmin=await user.findOne({email});
-  if(!logedAdmin){
+  const { email } = req.body;
+  const logedAdmin = await user.findOne({ email });
+  if (!logedAdmin) {
     return res.status(500).send("This email is not exist");
   }
-  if (await bcrypt.compare(req.body.password,logedAdmin.password)&&logedAdmin.isAdmin){
+  if (
+    (await bcrypt.compare(req.body.password, logedAdmin.password)) &&
+    logedAdmin.isAdmin
+  ) {
     return res.status(200).send("Welcome Admin :)");
-  }else{
-    return res.status(500).send("Make sure that your password true Or you are not Admin :( ")
+  } else {
+    return res
+      .status(500)
+      .send("Make sure that your password true Or you are not Admin :( ");
   }
 });
 
